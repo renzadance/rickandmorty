7b078795f23fbd7ddbb9d60fbb40c633eae9ea56
renzadance <bodalitvisenko@gmail.com>
2025-01-03 14:49:47 +0300

    installed shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,43 @@ export default {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        customGray: '#18191c',
-      },
-      backgroundImage: {
-        'hero-section-background-main': 'url(https://m.media-amazon.com/images/S/pv-target-images/3f8ae4a13de932bc679af5272ce983693d773818ff67a774dfcf0592bcd3beb7._SX1080_FMjpg_.jpg)',
-        'hero-section-background-characters': 'url(/characters-bg.jpg)',
-        'hero-section-background-episodes': 'url(/episodes-bg.jpg)', 
-      },
-    },
+  	extend: {
+  		colors: {
+  			customGray: '#18191c'
+  		},
+  		backgroundImage: {
+  			'hero-section-background-main': 'url(https://m.media-amazon.com/images/S/pv-target-images/3f8ae4a13de932bc679af5272ce983693d773818ff67a774dfcf0592bcd3beb7._SX1080_FMjpg_.jpg)',
+  			'hero-section-background-characters': 'url(/characters-bg.jpg)',
+  			'hero-section-background-episodes': 'url(/episodes-bg.jpg)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
+    plugins: [require("tailwindcss-animate")]
 } satisfies Config;
