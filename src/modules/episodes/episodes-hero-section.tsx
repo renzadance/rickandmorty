a707dea19e7765b5696aacd256fed0c0e0b11426
renzadance <bodalitvisenko@gmail.com>
2025-01-03 17:19:55 +0300

    animation herosection info

--- src/modules/episodes/episodes-hero-section.tsx
+++ src/modules/episodes/episodes-hero-section.tsx
@@ -1,3 +1,5 @@
+import NumberTicker from "@/components/ui/number-ticker";
+
 export function EpisodesHeroSection() {
   return (
     <div className="relative w-full h-[850px] bg-hero-section-background-episodes bg-cover bg-center flex items-end p-11">
@@ -23,10 +25,12 @@ export function EpisodesHeroSection() {
 
         <div className="flex gap-4 mt-4">
           <span className="text-neutral-400 text-sm bg-neutral-900 font-bold p-2 rounded">
-            51 Episodes
+            <NumberTicker className="text-neutral-400 text-sm" value={51} />{" "}
+            Episodes
           </span>
           <span className="text-neutral-400 text-sm bg-neutral-900 font-bold p-2 rounded">
-            5 Seasons
+            <NumberTicker className="text-neutral-400 text-sm" value={5} />{" "}
+            Seasons
           </span>
           <span className="text-neutral-400 text-sm bg-neutral-900 font-bold p-2 rounded">
             Multiverse Adventures
